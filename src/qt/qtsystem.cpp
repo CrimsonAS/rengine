1978f174397b13518f0babd4747ef1f8c9dccb72
Gunnar Sletta <gunnar.sletta@jollamobile.com>
2015-05-18 15:10:46 +0200

    compile (and crash) with Qt

--- src/qt/qtsystem.cpp
+++ src/qt/qtsystem.cpp
@@ -1,15 +1,26 @@
 #include "../system.h"
 
+#include <QGuiApplication>
+#include <QWindow>
+
 using namespace rengine;
 
-
+static int argc = 1;
+static const char *argv[] = { "fake_rengine_app" };
 
 class QtSystem : public System
 {
 public:
+    QtSystem()
+        : app(argc, (char **) argv)
+    {
+    }
+
     void run();
     Window *createWindow();
     Renderer *createRenderer(Window *window);
+
+    QGuiApplication app;
 };
 
 
@@ -17,19 +28,25 @@ public:
 class QtWindow : public Window
 {
 public:
+    QtWindow() {
+        window.resize(800, 480);
+    }
+
     void show() {
         printf("showing qt window...\n");
-        // window.show();
+        window.show();
     }
 
     void hide() {
         printf("hiding qt window...\n");
+        window.hide();
     }
 
     vec2 size() const {
-        return vec2(800, 600);
+        return vec2(window.width(), window.height());
     }
-    // QWindow window;
+
+    QWindow window;
 };
 
 
@@ -42,7 +59,7 @@ System *System::get()
 
 void QtSystem::run()
 {
-    // app.exec();
+    app.exec();
 }
 
 Window *QtSystem::createWindow()
