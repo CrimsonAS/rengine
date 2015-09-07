fb9c0a0549045912e0bc790ff49a21dc7f8469db
Gunnar Sletta <gunnar@sletta.org>
2015-09-07 16:57:58 +0200

    minor tweaks...

--- examples/benchmark_blend.cpp
+++ examples/benchmark_blend.cpp
@@ -1,3 +1,28 @@
+/*
+    Copyright (c) 2015, Gunnar Sletta <gunnar@sletta.org>
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice, this
+       list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright notice,
+       this list of conditions and the following disclaimer in the documentation
+       and/or other materials provided with the distribution.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+    ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+    ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+    ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 #include "rengine.h"
 #include "examples.h"
 
@@ -34,14 +59,12 @@ public:
 
         cout << "creating " << nodeCount << " layers.." << endl;
 
+        vec4 color((rand() % 100) / 100.0,
+                   (rand() % 100) / 100.0,
+                   (rand() % 100) / 100.0,
+                   0.9);
         rect2d geometry(0, 0, s.x, s.y);
-        vec4 color(1, 0, 0, 0.1);
         RectangleNode *root = RectangleNode::create(geometry, color);
-
-        for (int i=1; i<nodeCount; ++i) {
-            RectangleNode *child = RectangleNode::create(geometry, color);
-            *root << child;
-        }
 
         float bw = std::max(s.x, s.y) * 0.4;
         TransformNode *rotation = TransformNode::create(mat4::rotate2D(1));
@@ -50,6 +73,17 @@ public:
                         << RectangleNode::create(rect2d::fromXywh(-bw/2.0, -5, bw, 10), vec4(0, 0, 1, 1))
                        )
                   );
+
+        for (int i=1; i<nodeCount; ++i) {
+            geometry = rect2d(i * 4, i * 4, s.x, s.y);
+            color = vec4(1.0,
+                         1.0,
+                         1.0,
+                         0.1);
+            RectangleNode *child = RectangleNode::create(geometry, color);
+            *root << child;
+        }
+
 
         AnimationClosure<TransformNode, LinearTimingFunction> *anim
             = new AnimationClosure<TransformNode, LinearTimingFunction>(rotation);
