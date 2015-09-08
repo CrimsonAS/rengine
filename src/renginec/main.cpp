2827c172ecb4ff3b9edf32d251ff75ecfd98fb61
Gunnar Sletta <gunnar@sletta.org>
2015-09-08 23:24:26 +0200

    use picojson and save myself a bunch of hassle.

--- src/renginec/main.cpp
+++ src/renginec/main.cpp
@@ -1,28 +1,110 @@
-#include "tokenizer.h"
-#include "parser.h"
-#include "fstream"
+#include "picojson.h"
+#include <fstream>
 
 using namespace std;
+using namespace picojson;
 
 struct Options {
     string inputFile;
+    vector<string> includeDirs;
 } options;
 
-void printHelp(char *cmd)
+static void printHelp(char *cmd)
 {
     cout << "Usage:" << endl
          << " > " << cmd << "[options] inputfile" << endl
          << endl;
 }
 
+static bool loadFile(const string &file, value *v)
+{
+    cerr << "loading file: " << file << endl;
+    ifstream stream(file.c_str(), ifstream::in);
+    if (stream.eof()) {
+        cerr << "error(" << file << "): could not open file..." << endl;
+        return false;
+    }
+
+    stream >> *v;
+
+    string error = get_last_error();
+    if (!error.empty()) {
+        cerr << "error(" << file << "): " << error << endl;
+        return false;
+    }
+
+    if (!v->is<object>()) {
+        cerr << "error(" << file << "): no toplevel object..." << endl;
+        return false;
+    }
+
+    return true;
+}
+
+static bool fileExists(const std::string& name) {
+    ifstream f(name.c_str());
+    bool exists = f.good();
+    f.close();
+    return exists;
+}
+
+static bool loadFileInIncludeDirs(const std::string &file, value *v)
+{
+    for (auto i : options.includeDirs) {
+        *v = value();
+        std::string fileToTry = i + file;
+        if (fileExists(fileToTry) && loadFile(fileToTry, v))
+            return true;
+    }
+    return false;
+}
+
+static bool substituteImports(value *v)
+{
+    if (v->is<object>()) {
+        object &o = v->get<object>();
+
+        auto it = o.find("import");
+        if (it != o.end()) {
+            std::string importFile = it->second.get<std::string>();
+            o.erase(it);
+
+            value importValue;
+            if (!loadFileInIncludeDirs(importFile, &importValue)) {
+                cerr << "error: failed 'import' substitution of '" << importFile << "'" << endl;
+                return false;
+            }
+            if (importValue.is<object>()) {
+                for (auto impit : importValue.get<object>())
+                    o.insert(pair<string,value>(impit.first, impit.second));
+            }
+        } else {
+            for (auto i : o) {
+                bool ok = substituteImports(&i.second);
+                if (!ok)
+                    return false;
+            }
+        }
+    }
+    return true;
+}
+
 int main(int argc, char **argv)
 {
-    for (int i=0; i<argc; ++i) {
+    options.includeDirs.push_back("./");
+
+    for (int i=1; i<argc; ++i) {
         if (strstr(argv[i], "-h") != nullptr || strstr(argv[i], "--help")) {
             printHelp(argv[0]);
             return 0;
         } else {
-            options.inputFile = std::string(argv[i]);
+            std::string file(argv[i]);
+            options.inputFile = file;
+            size_t slash = file.find_last_of("/");
+            if (slash != string::npos) {
+                std::string dir = file.substr(0, slash+1);
+                options.includeDirs.push_back(dir);
+            }
         }
     }
 
@@ -31,11 +113,16 @@ int main(int argc, char **argv)
         return 0;
     }
 
-    cerr << "Reading file: " << options.inputFile << endl;
+    for (auto i : options.includeDirs)
+        cerr << "include dir: " << i << endl;
 
-    ifstream stream(options.inputFile.c_str(), ifstream::in);
-    Tokenizer tokenizer(stream);
+    value content;
+    if (!loadFile(options.inputFile, &content))
+        return 1;
 
-    Parser parser;
-    parser.parse(&tokenizer)
+    substituteImports(&content);
+
+    cout << content.serialize(true);
+
+    return 0;
 }
